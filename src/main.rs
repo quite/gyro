e0e8865e6ff608442195eba5573e6b1998d66160
Daniel Lublin <daniel@lublin.se>
2018-11-12 23:13:13 +0100

    Make it a minimal, responding ircbot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+use irc::error;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::load("config.toml").unwrap();
+    let mut reactor = IrcReactor::new().unwrap();
+    let client = reactor.prepare_client_and_connect(&config).unwrap();
+    client.identify().unwrap();
+    reactor.register_client_with_handler(client, process_msg);
+    reactor.run().unwrap();
+}
+
+fn process_msg(client: &IrcClient, message: Message) -> error::Result<()> {
+    print!("{}", message);
+    match message.command {
+        Command::PRIVMSG(ref _targ, ref msg) => {
+            if msg.contains("gyro") {
+                match message.response_target() {
+                    None => (),
+                    Some(ref t) => client.send_privmsg(t, "Hej!")?,
+                }
+            }
+        }
+        _ => (),
+    }
+    Ok(())
 }
